e28573022fee5e501ea53088f1a9efdaa81a7f7b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-12-16 05:54:41 +0000

    Dec 16, 2025, 5:54 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -2,5 +2,25 @@
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
 
+# comments start with hashtags
+
+def addition(num1, num2):
+    player.say(num1 * num2)
+
+addition(12, 9)
+
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+# tl : turn left
+def tl():
+    agent.turn(LEFT)
+player.on_chat("tl", tl)
+
+# tr: turn right
+def tr():
+    agent.turn(RIGHT)
+player.on_chat("tr", tr)
 
 ################## On Chat Commands Section #####################

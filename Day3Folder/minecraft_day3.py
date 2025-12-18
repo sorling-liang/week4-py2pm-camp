22bfb617e10876fda798936f4e571263be3d0961
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-12-18 05:57:40 +0000

    Dec 18, 2025, 5:57 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,63 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+# comments start with hashtags
+
+
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+# agent move forward
+def forward(count):
+    agent.move(FORWARD, count)
+player.on_chat("fw", forward)
+
+# BACK, "back"
+def back(count):
+    agent.move(BACK, count)
+player.on_chat("back", back)
+
+# UP, "up"
+def up(count):
+    agent.move(UP, count)
+player.on_chat("up", up)
+
+# DOWN, "down"
+def down(count):
+    agent.move(DOWN, count)
+player.on_chat("down", down)
+
+# tl : turn left
+def tl():
+    agent.turn(LEFT)
+player.on_chat("tl", tl)
+
+# tr: turn right
+def tr():
+    agent.turn(RIGHT)
+player.on_chat("tr", tr)
+
+
+
+def chop(ht):
+    for count in range(ht):
+        agent.destroy(FORWARD)
+        agent.collect_all()
+        agent.move(UP, 1)
+    agent.teleport_to_player()
+
+player.on_chat("chop", chop)
+
+
+def mine(length):
+    for count in range(length):
+        agent.destroy(FORWARD)
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+
+player.on_chat("destroy", mine)
+
+

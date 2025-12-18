aa8b304a6ad7a9e97cd1f4a9319fd81397770b60
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-12-18 07:22:45 +0000

    Dec 18, 2025, 7:22 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -6,7 +6,7 @@ print("hello from day4")
 # Task 1:
 
 import random
-print(random.randint(1, 3))
+print(random.randint(1, 999))
 
 ########################################################################
 # Task 2:

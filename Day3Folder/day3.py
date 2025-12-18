7a357a032f4a2d2f68d723b5092a03df1a1d1501
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-12-18 05:57:28 +0000

    Dec 18, 2025, 5:57 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,7 +5,12 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 
+# yourname = input("What is your name?")
+# title = input("What is your title?")
+# task = input("What is the task?")
 
+# print() use string concatenation +
+# print(title, yourname, "orders the peasants to", task)
 
 ########################################################################
 # Task 2:
@@ -15,6 +20,12 @@ print("hello from day3")
 ########################################################################
 # Task 3:
 
+# calculator
+# num1 = input("what is the first number?")
+# num2 = input("what is the second number?")
+# # conversion int()
+# answer = int(num1) * int(num2) 
+# print("the answer is", answer)
 
 
 ########################################################################
@@ -25,7 +36,20 @@ print("hello from day3")
 ########################################################################
 # Task 5:
 
+hidden_password = "passme"
+is_correct = False
+for count in range(3):
+    guess = input("do you know my password?")
 
+    if hidden_password == guess:
+        print("please come in")
+        is_correct = True
+        break
+    else:
+        print("its okay, try again!")
+
+if is_correct == False:
+    print("your account is locked. I have recorded your face in the video")
 
 ########################################################################
 # Task 6:
